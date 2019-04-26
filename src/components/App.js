5133c7b0734a578633f8d63f75b0c2fb00bc7e64
jmaddox <jmaddox@ncsoft.com>
2019-04-25 18:32:24 -0700

    Inital setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../global.scss';
+
+//import sections
+
+function App() {
+  return (
+    <div className="app">
+      Placeholder
+    </div>
+  );
+}
+
+export default App;
